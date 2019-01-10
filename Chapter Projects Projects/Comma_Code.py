c040f9be8056708fdba5ee45a943189441810b7e
rochejohn <john.a.roche@outlook.com>
2019-01-10 15:28:48 +1000

    update structure

---
+++ Chapter Projects Projects/Comma_Code.py
@@ -0,0 +1,23 @@
+'''
+Automate the boring stuff chapter 4
+Project: Comma Code
+Write a function that takes a list value as an argument and
+returns a string with all the items separated by a comma and a space,
+with and inserted before the last item.
+'''
+
+
+
+
+
+def comma_code(lst):
+    string1=''
+    for item in range(0,len(lst)-1):
+        string1 += lst[item].title() + ', '  #added title method for nicer look
+
+    print(string1 + 'and ' + lst[-1])
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+comma_code(spam)
---
+++ Chapter Projects Projects/collatz.py
@@ -0,0 +1,39 @@
+def collatz(number):
+
+
+
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    else:
+        return 3 * number + 1
+
+
+
+def game():
+
+    try:
+        number = int(input('Please enter a number: '))
+        while number != 1:
+            number = collatz(number)
+        play()
+
+    except ValueError:
+        print('Please enter a valid number!')
+        game()
+
+def play():
+    ans = input('Try again? Y or N:\n').upper()
+
+    if ans == 'Y':
+        game()
+
+    elif ans == 'N':
+        print('GoodBye!')
+
+    else:
+        print('Invalid Option')
+        play()
+
+game()
