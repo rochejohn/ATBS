0f84a52db861578daaffeadbabed29d74a3d24cc
rochejohn <john.a.roche@outlook.com>
2019-01-10 15:21:04 +1000

    Chapter 3 Project collatz Final

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -1,16 +1,27 @@
 def collatz(number):
+
+
+
     if number % 2 == 0:
         print(number // 2)
         return number // 2
+
     else:
         return 3 * number + 1
 
-def game():
-    number = int(input('Please enter a number: '))
-    while number != 1:
-        number = collatz(number)
-    play()
 
+
+def game():
+
+    try:
+        number = int(input('Please enter a number: '))
+        while number != 1:
+            number = collatz(number)
+        play()
+
+    except ValueError:
+        print('Please enter a valid number!')
+        game()
 
 def play():
     ans = input('Try again? Y or N:\n').upper()
