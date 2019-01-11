bc933317b7dacd985c2f0429b79d55c88f144c64
rochejohn <john.a.roche@outlook.com>
2019-01-11 13:22:42 +1000

    Chapter 5 Fantasy Game Inventory Part 2 added (fallen dragon items)

--- Chapter Projects Projects/Fantasy_Game_Inventory.py
+++ Chapter Projects Projects/Fantasy_Game_Inventory.py
@@ -1,4 +1,4 @@
-
+'''Part One'''
 
 bagpack = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
@@ -22,3 +22,32 @@ def displayInventory(inventory):
 
 
 displayInventory(bagpack)
+
+'''
+Part 2
+
+Add a fallen dragons list of items to your bagpack
+'''
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item,0)
+        inventory[item] += 1
+
+addToInventory(bagpack,dragonLoot)
+
+displayInventory(bagpack)
+
+
+
+
+
+
+
+
+
+
+
