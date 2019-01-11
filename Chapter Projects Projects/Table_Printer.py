eef6b12c294af81666fae9d04b2ba458306aad18
rochejohn <john.a.roche@outlook.com>
2019-01-11 17:23:37 +1000

    Chapter 6 Table Printer Finished (testing)

--- Chapter Projects Projects/Table_Printer.py
+++ Chapter Projects Projects/Table_Printer.py
@@ -4,15 +4,26 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 
-def table_printer(lst):
-    colWidths = [0] * len(tableData)
-    for outlist in range(len(tableData)):
+def table_printer(bag):
+    colWidths = [0] * len(bag)
+    for outlist in range(len(bag)):
         mx = 0
-        for innerlist in range(len(tableData[outlist])):
-            if len(tableData[outlist][innerlist]) > mx:
-                mx = len(tableData[outlist][innerlist])
+        for innerlist in range(len(bag[outlist])):
+            if len(bag[outlist][innerlist]) > mx:
+                mx = len(bag[outlist][innerlist])
         colWidths[outlist] = mx
-    return colWidths
 
-print(table_printer(tableData))
 
+
+    for i in range(len(bag[0])):
+
+
+        for o in range(len(bag)):
+
+
+            print(bag[o][i].rjust(colWidths[o]), end=" ")
+
+        print("")
+
+
+table_printer(tableData)
