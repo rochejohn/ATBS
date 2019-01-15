5956a1274a588d73ed463d99c52112557afa5ff3
rochejohn <john.a.roche@outlook.com>
2019-01-15 13:14:21 +1000

    Regex_strip. review ^$ and whitespace

--- Chapter Projects Projects/Regex_strip.py
+++ Chapter Projects Projects/Regex_strip.py
@@ -4,7 +4,7 @@ If no other arguments are passed other than the string to strip, then whitespace
 be removed from the beginning and end of the string. Otherwise, the characters specified in the
 second argument to the function will be removed from the string.
 '''
-import re
+import re,time
 
 '''beginning and end ^ $ multiple times'''
 
@@ -15,25 +15,110 @@ def regex_strip(string, strip = 'default'):
     #print(strip)
 
     whitespace = re.compile(r'^\s*|\s*$')
-    user_def = re.compile(r'strip')
+    user_def = re.compile(strip)
 
     if strip == 'default':
-        sting_stripped = whitespace.sub('',string)
-        return sting_stripped
+        string_stripped = whitespace.sub('',string)
+        return string_stripped
 
     else:
-        return none
+        string_stripped = user_def.sub('',string)
+        return string_stripped
 
-print('    hello world    ')
-print(len('    hello world    '))
+####################################################################################################
+####################################################################################################
 
-a = regex_strip('    hello world    ')
-print(len(a))
+# Example one, take argument 'a' and remove from string
 
-b = '          john          '
-print(len(b))
-c = regex_strip(b)
-print(len(c))
+string_a = 'aaaajohnaaaa'
+#12 Char long // will remove all 'a'
+print()
+print('Example 1',end=' ')
+print(string_a)
+#print out example
+
+print('The length =', end=' ')
+print(len(string_a))
+# prints length of 12 before remove with strip
+
+string_a = (regex_strip(string_a,'a'))
+#funtion called
+print()
+print('Example 1 stripped:',end=' ')
+print(string_a)
+#print out after strip
+
+print('The length =', end=' ')
+print(len(string_a))
+# function takes in 'string a' and the parameter 'a' to use for strip // returns john with
+# all 'a' removed from beginning and end of string
+
+print()
+print('####################################################################################################')
+print()
+####################################################################################################
+####################################################################################################
+
+# Example two, no argument given, defaults to remove whitespace
+
+string_whitespace = '          john    '
+
+print('Example 2',end=' ')
+print(string_whitespace)
+#print out example
+
+print('The length =', end=' ')
+print(len(string_whitespace))
+# prints length of example before remove with strip
+
+string_whitespace = regex_strip(string_whitespace)
+#note no argument given for second placeholder
+#funtion called!
+print()
+print('Example 2',end=' ')
+print(string_whitespace)
+#print out after strip
+
+print('The length =', end=' ')
+print(len(string_whitespace))
+# function takes in 'string a' and the parameter 'a' to use for strip // returns john with
+# all 'a' removed from beginning and end of string
+
+print()
+print('####################################################################################################')
+print()
+
+while True:
+
+    ans = input('Would you like to test with an example? y/n: ')
+
+    if ans.lower() == 'y':
+        supplied_string = input('Please type your string: ')
+        supplied_strip = input('Please supply the parameter to strip. (Leave blank for whitespace): ')
+        len_supplied_string = len(supplied_string)
+        print('Your input string is "%s" with a length of "%s"' %(supplied_string,len_supplied_string))
+
+        print('Applying strip function as per your supplied parameters...')
+        time.sleep(2)
+
+        stripped_string = regex_strip(supplied_string,supplied_strip)
+        len_stripped_string = len(stripped_string)
+        print('Your stripped string is "%s" with a length of "%s"' %(stripped_string,len_stripped_string))
+        continue
+
+
+
+    elif ans == 'n':
+        print('Goodbye for now!')
+        break
+
+    else:
+        print('Invalid option! Please enter y/n....')
+
+
+
+
+#whitespacebug and whitespaces empty
 
 
 
