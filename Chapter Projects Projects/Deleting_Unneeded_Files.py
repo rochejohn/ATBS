08e1df85b84dbc5f1c8736ab2a0721a0dda66a4f
rochejohn <john.a.roche@outlook.com>
2019-01-18 11:47:06 +1000

    Deleting Unneeded Files Start

---
+++ Chapter Projects Projects/Deleting_Unneeded_Files.py
@@ -0,0 +1,6 @@
+'''
+Write a program that walks through a folder tree and searches
+for exceptionally large files or folders—say, ones that have a file size of more than 100MB.
+(Remember, to get a file’s size, you can use os.path.getsize() from the os module.)
+Print these files with their absolute path to the screen.
+'''
