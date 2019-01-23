cd5bc0e437c00c6c4386b6b2c2e865c2a953cd60
rochejohn <john.a.roche@outlook.com>
2019-01-23 17:00:09 +1000

    Imgur Search. Restart due to switch to Selenium

--- Chapter Projects Projects/Image_Site_Downloader.py
+++ Chapter Projects Projects/Image_Site_Downloader.py
@@ -10,29 +10,11 @@ Will use imgur site
 Allow user to search for own category
 Create folder to download up to 10 images only (set limit)
 Example to be used (Dog Memes from Imgur)
-Can use Selenium or Requests/BS4 
-Using BS4 so no browser needs to be opened!
 '''
+#####SWITCHING TO SELENIUM AS REQUESTS WONT LOAD JS AND TAGS NOT FOUND IN BS4
 
 
-'''os for making dir and os path for file paths and names'''
-'''requests to download html page, and images'''
-'''bs4 to parse and search for html elements, next button and images'''
 
-import logging, os, requests, bs4
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
-#TODO change below to variable so user can enter own search
-request_imgur_home_page = requests.get('https://imgur.com/search?q=dog%20meme')        # make request object for imgur homepage
-request_imgur_home_page.raise_for_status()                         # check if any issues getting page
 
-soup_imgur = bs4.BeautifulSoup(request_imgur_home_page.text,features='html.parser')       # create the bs4 object from the requests objects attribute text(where html text is stored as a string)
-
-soup_elem = soup_imgur.select('d > a')
-
-logging.debug(type(soup_elem))
-logging.debug(len(soup_elem))
-logging.debug(soup_elem[0])
-
-logging.debug(request_imgur_home_page.text[0:200])
