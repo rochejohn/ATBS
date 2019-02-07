406e6bacf26a7f8b92f5c00a3bc50639f41ef3cb
rochejohn <john.a.roche@outlook.com>
2019-02-08 00:19:43 +1000

    Final Commit, updated Directory name.

--- Projects/Umbrella.py
+++ Projects/Umbrella.py
@@ -38,14 +38,14 @@ from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACda2ce57dcdf712414bd13779f3b20331'
-auth_token = '1a42f68dd2e333f7513bc6368fd80bdc'
+account_sid = '######'
+auth_token = '#####'
 client = Client(account_sid, auth_token)
 
 
 my_text = weather_output
 
-message = client.messages.create(body=my_text, from_='+61488856205', to='+61435739754')
+message = client.messages.create(body=my_text, from_='#####', to='######')
 
 
 
