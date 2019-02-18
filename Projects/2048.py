df6dc30047c62f61af595b37c7438944abd37512
rochejohn <john.a.roche@outlook.com>
2019-02-18 17:55:40 +1000

    added #! /usr/bin/env python3 for venv to 2048.py

--- Projects/2048.py
+++ Projects/2048.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python3
+
+
 '''
 2048 is a simple game where you combine tiles by sliding them up, down, left, or right with
 the arrow keys. You can actually get a fairly high score by repeatedly sliding in an up, right, down,
